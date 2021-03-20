83caa5d70aa78d85f108156bb3ae4ab0836f0060
Gersón Gómez <gergomez18@gmail.com>
2021-03-20 18:57:33 -0500

    Actualización

--- layouts/head.php
+++ layouts/head.php
@@ -15,8 +15,19 @@
 		<link rel="stylesheet" href="https://cdn.codemonkey.com.mx/css/valkyrie.css" type="text/css" media="all" />
 		<link rel="stylesheet" href="{$path.css}styles.css?v=1.0" type="text/css" media="all" />
 		{$dependencies.css}
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-WVX66WW');</script>
+		<!-- End Google Tag Manager -->
 	</head>
 	<body>
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WVX66WW"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
 		<header class="desktop_header p-t-40 p-b-40" style="width:100%;position:fixed;transition: 400ms;z-index:99;">
 			<div class="container">
 				<div class="row d-flex align-items-center">
